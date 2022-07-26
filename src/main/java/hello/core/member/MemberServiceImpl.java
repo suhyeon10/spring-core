ea69c34dd2011103c456a27590d581d54ccf93a9
장수현 <76679995+suhyeon10@users.noreply.github.com>
2022-07-27 00:21:52 +0900

    회원 도메인 개발

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,15 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member); //다형성에 의해 메모리멤버리포지토리의 save가 호출됨
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
