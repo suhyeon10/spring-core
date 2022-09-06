09247e0050ab74bb0cbcb23c6f49a66a3e7c27a1
장수현 <76679995+suhyeon10@users.noreply.github.com>
2022-09-06 22:49:18 +0900

    Appconfig refactoring

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,12 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
     @Override
     public void join(Member member) {
         memberRepository.save(member); //다형성에 의해 메모리멤버리포지토리의 save가 호출됨
